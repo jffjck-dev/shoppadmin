267022260b7f636ba8abb68322f3bec155420384
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-07-03 12:39:42 +0200

    config: add shadcn-ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div className="p-4">
+            <p>Admin Dashboard</p>
+            <Button size="sm" variant="destructive">Click Me</Button>
+        </div>
+    )
+}
