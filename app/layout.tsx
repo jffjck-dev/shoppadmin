0230d5b044b6cb90fc594724b5c24e9244b75832
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-07-04 13:44:35 +0200

    feature: create Modal, StoreModal, useStoreModal and ModalProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 
-const inter = Inter({ subsets: ['latin'] })
+import {ModalProvider} from "@/providers/modal-provider";
+
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'ShoppAdmin - Dashboard',
   description: 'Admin Dashboard',
-}
+};
 
 export default function RootLayout({
   children,
@@ -17,8 +20,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ModalProvider />
+        {children}
+      </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
