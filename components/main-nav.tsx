ab93f32fccc4fa430843794f5373121207c05342
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-07-06 00:59:45 +0200

    fix: unused props parameters (missing props drilling)

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -32,7 +32,7 @@ export function MainNav({
     ];
 
     return (
-        <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+        <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)} {...props}>
             {routes.map((route) => (
                 <Link
                     key={route.href}
