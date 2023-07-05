fee8997650e6403e5a376e3e7e9cef1e4a9e000d
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-07-05 09:50:09 +0200

    feature: add DashboardPage/Layout + update SetupPage/Layout

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -33,6 +33,7 @@ export const StoreModal = () => {
         try {
             setLoading(true);
             const response = await axios.post('/api/stores', values);
+            window.location.assign(`/${response.data.id}`);
             toast.success("Store created!");
         } catch(err) {
             toast.error("Something went wrong...");
