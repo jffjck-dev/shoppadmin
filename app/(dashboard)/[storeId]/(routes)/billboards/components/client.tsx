72918046d243c18896d89ec23f010c23ceb5bb0e
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-07-06 18:14:04 +0200

    feat: add tables and API routes list to billboard page

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,21 +6,33 @@ import {Plus} from "lucide-react";
 import {Heading} from "@/components/ui/heading";
 import {Button} from "@/components/ui/button";
 import {Separator} from "@/components/ui/separator";
+import {DataTable} from "@/components/ui/data-table";
+import {APiList} from "@/components/ui/api-list";
 
-export const BillboardClient = () => {
+import {BillboardColumn, columns} from "./column";
+
+interface BillboardClientProps {
+    data: BillboardColumn[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
     const router = useRouter();
     const params = useParams();
 
     return (
         <>
             <div className="flex items-center justify-between">
-                <Heading title="Billboard (0)" description="Manage billboards for your store" />
+                <Heading title={`Billboard (${data.length})`} description="Manage billboards for your store" />
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
                     Add new
                 </Button>
             </div>
             <Separator />
+            <DataTable columns={columns} data={data} searchKey="label"/>
+            <Heading title="API" description="API calls for Billboards" />
+            <Separator />
+            <APiList entityName="billboards" entityIdName="billboardId" />
         </>
     )
 }
