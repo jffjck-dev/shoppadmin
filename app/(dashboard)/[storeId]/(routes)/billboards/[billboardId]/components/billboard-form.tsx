be094b8e2d7c1a9d7bb69c691267bd83a7397513
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-07-06 18:10:55 +0200

    fix: BillboardForm change redirection

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -72,7 +72,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({initialData}) => {
             setLoading(true);
             await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
             router.refresh();
-            router.push('/');
+            router.push(`/${params.storeId}/billboards`);
             toast.success("Billboard deleted!");
         } catch(err) {
             toast.error("Make sure you removed all categories using this billboard first.");
@@ -122,7 +122,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({initialData}) => {
                     <Button type="submit" disabled={loading} className="ml-auto">{action}</Button>
                 </form>
             </Form>
-            <Separator />
         </>
     );
 }
