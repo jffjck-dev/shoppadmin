6d92546d8c82e99f65d818f63584491d75b52dcb
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-07-12 15:07:08 +0200

    feat: add checkout route + setup Stripe

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -2,6 +2,28 @@ import {NextResponse} from "next/server";
 import {auth} from "@clerk/nextjs";
 import prismadb from "@/lib/prismadb";
 
+export async function GET(
+    req: Request,
+    { params }: { params: { categoryId: string } }
+) {
+    try {
+        if (!params.categoryId) {
+            return new NextResponse("Category id is required", { status: 400 });
+        }
+
+        const category = await prismadb.category.findUnique({
+            where: {
+                id: params.categoryId,
+            },
+        });
+
+        return NextResponse.json(category);
+    } catch (error) {
+        console.log('[CATEGORY_GET]', error);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+}
+
 export async function PATCH (
     req: Request,
     { params }: {params: {storeId: string }}
